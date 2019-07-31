09f90c02fa89c6bcc1cfa1eb52e0b978ff9ce6be
sumitkute <sumit_kute@yahoo.com>
2019-07-31 11:34:42 +0530

    Add project files.

---
+++ TalentLeaveRound/TalentLeaveRound.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+
+
+namespace TalentLeaveRound
+{
+    public static class TalentLeaveRound
+    {
+        [FunctionName("TalentLeaveRound")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+            //JObject talentreq = null;
+            HttpResponseMessage responseMessage = new HttpResponseMessage();
+            // Get request body
+            dynamic data = await req.Content.ReadAsAsync<object>();
+            double amount = data.Amount;
+            amount = Math.Round(amount, 0);
+            //12.54 == 12.51
+
+            if (amount > 0)
+            {
+                responseMessage = req.CreateResponse(HttpStatusCode.OK);
+                responseMessage.Content = new StringContent($"The passed amount is : {amount.ToString()}", System.Text.Encoding.UTF8, "application/json");
+            }
+            return amount <= 0
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Not a valid request")
+                : responseMessage;
+
+        }
+    }
+}
